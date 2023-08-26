3b0aa3b8512b0826699b2c3f16c3b958562b450a
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-08-25 23:04:57 -0400

    added questions to index.js

--- index.js
+++ index.js
@@ -4,7 +4,31 @@ const fs = require('fs');
 const generateMarkDown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
-
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why and how of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.'
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -18,8 +42,8 @@ function init() {
             const markDownContent = generateMarkDown(answers);
 
             fs.writeFile('markDown.md', markDownContent, (err) =>
-            err ?console.log(err)
-            :console.log('Successfully created markDown.md!')
+                err ? console.log(err)
+                    : console.log('Successfully created markDown.md!')
             );
         });
 }
